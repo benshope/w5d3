925627663b990a0ebb3b9339c595be2dcca6188f
benshope <nimajnebs@gmail.com>
2013-04-10 09:25:37 -0700

    Add app

---
+++ NewsApp/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+NewsApp::Application.config.secret_token = '06e5a0822bcad0f019787b754d38c70a250295e23e3150bf8d68210952447fcba9170c96e30f1326e93a3c3783da18c16d9611dac3e2d60b7618c0011938c9ba'
